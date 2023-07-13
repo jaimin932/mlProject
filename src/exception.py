d2122bb0a97c550a7ad7d8166b15502b9539bfdb
unknown <shahjaimin634@gmail.com>
2023-07-13 14:55:26 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_msg_detail(error,error_detail:sys):
     return error_message
    
 
-class CustomExceptrion(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_msg_detail(error_message,error_detail=error_detail)
@@ -17,9 +17,3 @@ class CustomExceptrion(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try: 
-        a =1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomExceptrion(e,sys)
